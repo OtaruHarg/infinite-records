45ae0dd6241ef69f474f204757d3640383708ec8
小樽端倶(AxolotlProject) <otarutrpg@gmail.com>
2026-02-15 11:45:52 +0900

    Update script_index.js

--- script_index.js
+++ script_index.js
@@ -1,21 +1,22 @@
-var result;
-
-
-function starts() {
-    result = ['利用規約に同意しますか？', '規約を読んでいない場合はキャンセルを押してください'];
-    if(!window.confirm(result.join('\n'))){
-        return false;
-    }
-    if(result){
-        window.location.href = "main.html";
-    }
-}
+var erement;
 
 function starts(){
     document.querySelector("#alert_a").textContent = "利用規約に同意しますか？";
     document.querySelector("#alert_b").textContent = "規約を読んでいない場合はキャンセルを押してください。";
     erement = document.getElementById("alertbord");
-    erement.style.opacity = 1;
+    console.log(erement);
+    erement.style.opacity = "1";
     erement.style.zIndex = 100;
     erement.style.position = "relative";
+}
+
+function cancel(){
+    erement = document.getElementById("alertbord");
+    erement.style.opacity = 0;
+    erement.style.zIndex = -10;
+    erement.style.position = "relative";
+}
+
+function does(){
+    window.location.href = "main.html";
 }
